82ee72862d063e32a8f7d913a8d3f7644cae2b35
kenji955 <blelm1.sk@gmail.com>
2020-06-28 17:53:18 +0900

    アプリ用フォルダ 初回アップロード⇒コミット先変更のため取り消し

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+import MainNavigation from "./shared/Navigation/MainNavigation";
+
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const todoAddHandler = (text: string) => {
+        setTodos((prevTodos) => [
+            ...prevTodos,
+            { id: Math.random().toString(), text: text },
+        ]);
+    };
+
+    const todoDeleteHandler = (todoId: string) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter((todo) => todo.id !== todoId);
+        });
+    };
+
+    return (
+        <React.Fragment>
+            <div style={{ display: "inline-block" }}>
+                <Router>
+                    <MainNavigation />
+                </Router>
+            </div>
+            <div className="App">
+                <NewTodo onAddTodo={todoAddHandler} />
+                <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+            </div>
+        </React.Fragment>
+    );
+};
 
 export default App;
